d9b9ed7db3bccf7e0fe699dc61860f1a003a8643
Tyr Chen <tyr.chen@gmail.com>
2019-02-05 11:45:09 -0800

    update the version of the abi repo

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,25 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["mix.exs", "{config,lib,test,priv}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    plug: :*,
+    pipe_through: :*,
+    forward: :*,
+    forward: :*,
+    get: :*,
+    post: :*,
+    delete: :*,
+    patch: :*,
+    socket: :*,
+    socket: :*,
+    field: :*,
+    oneof: :*,
+    rpc: :*,
+    object: :*,
+    input_object: :*,
+    field: :*,
+    arg: :*,
+    resolve: :*,
+    value: :*
+  ]
 ]
--- mix.exs
+++ mix.exs
@@ -1,28 +1,27 @@
 defmodule ForgeAbi.MixProject do
   use Mix.Project
+  @top File.cwd!()
+
+  @version @top |> Path.join("version") |> File.read!() |> String.trim()
+  @elixir_version @top |> Path.join(".elixir_version") |> File.read!() |> String.trim()
 
   def project do
     [
       app: :forge_abi,
-      version: "0.1.0",
-      elixir: "~> 1.8",
+      version: @version,
+      elixir: @elixir_version,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end
