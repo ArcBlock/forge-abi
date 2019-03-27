8aaf63cb6d144becf9f659576d1ea80d70cf75a1
Tyr Chen <tyr.chen@gmail.com>
2019-03-27 07:47:57 -0700

    fix protobuf to use 0.5.3 (0.6.x not working with forge now)

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule ForgeAbi.MixProject do
     [
       {:ex_abci_proto, "~> 0.8"},
       {:jason, "~> 1.1"},
+      {:protobuf, "~> 0.5.3", override: true},
       {:typed_struct, "~> 0.1.4"},
 
       # dev and test
