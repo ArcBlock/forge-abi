7beb968ef9e75c997baaf2b43ff3fedaf57688b7
Lei Zhou <lei@arcblock.io>
2019-04-05 11:00:40 -0700

    use blockInfo for now and set default value to txInfo

--- lib/protobuf/gen/rpc.pb.ex
+++ lib/protobuf/gen/rpc.pb.ex
@@ -173,13 +173,13 @@ defmodule ForgeAbi.ResponseGetBlocks do
   @type t :: %__MODULE__{
           code: integer,
           page: ForgeAbi.PageInfo.t(),
-          blocks: [ForgeAbi.BlockInfoSimple.t()]
+          blocks: [ForgeAbi.BlockInfo.t()]
         }
   defstruct [:code, :page, :blocks]
 
   field :code, 1, type: ForgeAbi.StatusCode, enum: true
   field :page, 2, type: ForgeAbi.PageInfo
-  field :blocks, 3, repeated: true, type: ForgeAbi.BlockInfoSimple
+  field :blocks, 3, repeated: true, type: ForgeAbi.BlockInfo
 end
 
 defmodule ForgeAbi.RequestCreateWallet do
