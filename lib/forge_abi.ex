66ae842a11d3c00ec74aa6b233e6cdd56ab8c289
Tyr Chen <tyr.chen@gmail.com>
2019-04-19 18:17:33 -0700

    support deploy tx (#63)
    * support deploy tx
    
    * support tx protocol state
    
    * add namespace
    
    * add role type role_tx
    
    * rename deploy protocol tx
    
    * update protocols
    
    * add better comments
    
    * fix typo
    
    * add context to protocol state.
    
    * add migrated fields
    
    * add get protocol rpc
    
    * add new rpc in state rpc
    
    * add ProtocolStatus
    
    * add module name into the CodeInfo
    
    * update code info
    
    * use checksum
    
    * support protocol list state
    
    * add protocol list in forge state
    
    * remove deploy protocol
    
    * remove tx
    
    * add declare and deploy tx back
    
    * fix forge-abi
    
    * simplify type_urls
    
    * fix type url
    
    * add type urls in proto
    
    * fix type url test cases
    
    * put hash in the state, so we can retrieve it directly later on
    
    * add declare type url
    
    * change encode to encode_any, decode to decode_any
    
    * support get one type_url
    
    * typo
    
    * fix generated tx.yml
    
    * fix tx.yml
    
    * fix yaml
    
    * rollback sys upgrade and consensus upgrade
    
    * maintain the same order
    
    * fix abi generated yml
    
    * add wallet type back to wallet info
    
    * rollback wallet type
    
    * use topic name instead of topic type
    
    * update forge state
    
    * update yaml
    
    * remove get asset address and sign data
    
    * add the tx support back

--- lib/forge_abi.ex
+++ lib/forge_abi.ex
@@ -5,10 +5,16 @@ defmodule ForgeAbi do
   alias ForgeAbi.Util.TypeUrl
 
   # typeurl related
-  defdelegate encode_any(type, data), to: TypeUrl, as: :encode
-  defdelegate encode_any!(type, data), to: TypeUrl, as: :encode!
-  defdelegate decode_any(any), to: TypeUrl, as: :decode
-  defdelegate register_type_urls(urls), to: TypeUrl, as: :extend
+  defdelegate add_type_url(type_url, mod), to: TypeUrl, as: :add
+  defdelegate add_type_urls(type_urls), to: TypeUrl, as: :add
+  defdelegate remove_type_url(type_url), to: TypeUrl, as: :remove
+  defdelegate get_type_url(v), to: TypeUrl, as: :get
+  defdelegate get_type_urls, to: TypeUrl, as: :all
+
+  defdelegate encode_any(data, type_url \\ nil), to: TypeUrl
+  defdelegate encode_any!(data, type_url \\ nil), to: TypeUrl
+  defdelegate decode_any(any), to: TypeUrl
+  defdelegate decode_any!(any), to: TypeUrl
 
   # unit related
   defdelegate one_token, to: ForgeAbi.Unit
