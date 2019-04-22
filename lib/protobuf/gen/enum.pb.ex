8ca20fbfe76226b0875d469cd0bdad52c7de8bf8
Peiling Ding <dingpl716@gmail.com>
2019-04-22 16:48:47 -0700

    Add invalid_lock_status enum.

--- lib/protobuf/gen/enum.pb.ex
+++ lib/protobuf/gen/enum.pb.ex
@@ -14,6 +14,7 @@ defmodule ForgeAbi.StatusCode do
   field :unsupported_tx, 9
   field :expired_tx, 10
   field :too_many_txs, 11
+  field :invalid_lock_status, 12
   field :invalid_moniker, 16
   field :invalid_passphrase, 17
   field :invalid_multisig, 20
