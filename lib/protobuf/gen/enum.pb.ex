754b6ca3df8b9e9543f9c1cc83274eb352351269
Peiling Ding <dingpl716@gmail.com>
2019-05-07 15:12:38 -0700

    Add error code for withdrawing tether transaction. (#83)
    * Add error code for withdrawing tether transaction.
    
    * Restrict the number of tether to be one in exchange tx.
    
    * Add invalid custodian error code.

--- lib/protobuf/gen/enum.pb.ex
+++ lib/protobuf/gen/enum.pb.ex
@@ -45,6 +45,8 @@ defmodule ForgeAbi.StatusCode do
   field :invalid_withdrawer, 47
   field :duplicate_tether, 48
   field :invalid_expiry_date, 49
+  field :invalid_deposit, 50
+  field :invalid_custodian, 51
   field :forbidden, 403
   field :internal, 500
   field :timeout, 504
