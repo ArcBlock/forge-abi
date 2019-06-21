55de8b9927253d0b3c8640a00b4793da38565d11
sunboshan <sunboshan@users.noreply.github.com>
2019-06-21 13:42:31 -0700

    Add gas to tx (#94)
    * Add gas in AccountState and Transaction
    
    * Add insufficient_gas status code
    
    * Add gas to ForgeState
    
    * Use uint32 for map

--- lib/protobuf/gen/state.pb.ex
+++ lib/protobuf/gen/state.pb.ex
@@ -12,6 +12,7 @@ defmodule ForgeAbi.AccountState do
           moniker: String.t(),
           context: ForgeAbi.StateContext.t() | nil,
           issuer: String.t(),
+          gas_balance: ForgeAbi.BigUint.t() | nil,
           migrated_to: [String.t()],
           migrated_from: [String.t()],
           num_assets: non_neg_integer,
@@ -31,6 +32,7 @@ defmodule ForgeAbi.AccountState do
     :moniker,
     :context,
     :issuer,
+    :gas_balance,
     :migrated_to,
     :migrated_from,
     :num_assets,
@@ -50,6 +52,7 @@ defmodule ForgeAbi.AccountState do
   field :moniker, 7, type: :string
   field :context, 8, type: ForgeAbi.StateContext
   field :issuer, 9, type: :string
+  field :gas_balance, 10, type: ForgeAbi.BigUint
   field :migrated_to, 13, repeated: true, type: :string
   field :migrated_from, 14, repeated: true, type: :string
   field :num_assets, 15, type: :uint64
@@ -137,6 +140,7 @@ defmodule ForgeAbi.ForgeState do
           stake_config: ForgeAbi.StakeConfig.t() | nil,
           poke_config: ForgeAbi.PokeConfig.t() | nil,
           protocols: [ForgeAbi.CoreProtocol.t()],
+          gas: %{String.t() => non_neg_integer},
           upgrade_info: ForgeAbi.UpgradeInfo.t() | nil,
           data: Google.Protobuf.Any.t() | nil
         }
@@ -152,6 +156,7 @@ defmodule ForgeAbi.ForgeState do
     :stake_config,
     :poke_config,
     :protocols,
+    :gas,
     :upgrade_info,
     :data
   ]
@@ -167,6 +172,7 @@ defmodule ForgeAbi.ForgeState do
   field :stake_config, 10, type: ForgeAbi.StakeConfig
   field :poke_config, 11, type: ForgeAbi.PokeConfig
   field :protocols, 12, repeated: true, type: ForgeAbi.CoreProtocol
+  field :gas, 13, repeated: true, type: ForgeAbi.ForgeState.GasEntry, map: true
   field :upgrade_info, 14, type: ForgeAbi.UpgradeInfo
   field :data, 15, type: Google.Protobuf.Any
 end
@@ -197,6 +203,20 @@ defmodule ForgeAbi.ForgeState.StakeSummaryEntry do
 
   field :key, 1, type: :uint32
   field :value, 2, type: ForgeAbi.StakeSummary
+end
+
+defmodule ForgeAbi.ForgeState.GasEntry do
+  @moduledoc false
+  use Protobuf, map: true, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          key: String.t(),
+          value: non_neg_integer
+        }
+  defstruct [:key, :value]
+
+  field :key, 1, type: :string
+  field :value, 2, type: :uint32
 end
 
 defmodule ForgeAbi.RootState do
