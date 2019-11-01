81c606aa95be18c3be1e857d9bfb8d8e85c3c767
Tyr Chen <tyr.chen@gmail.com>
2019-11-01 14:08:53 -0700

    add more test cases

--- lib/forge_abi/util/common.ex
+++ lib/forge_abi/util/common.ex
@@ -5,6 +5,44 @@ defmodule ForgeAbi.Util do
 
   @doc """
   Retrieve the nested default value for mod
+
+  Examples:
+
+    iex> ForgeAbi.Util.get_default(ForgeAbi.DelegateOpState)
+    %ForgeAbi.DelegateOpState{
+      balance: %ForgeAbi.BigUint{value: ""},
+      balance_delta: %ForgeAbi.BigUint{value: ""},
+      num_txs: 0,
+      num_txs_delta: 0,
+      rule: ""
+    }
+
+    iex> ForgeAbi.Util.get_default(ForgeAbi.TransactionInfo)
+    %ForgeAbi.TransactionInfo{
+      code: :ok,
+      hash: "",
+      height: 0,
+      index: 0,
+      tags: %AbciVendor.KVPair{key: "", value: ""},
+      time: %Google.Protobuf.Timestamp{nanos: 0, seconds: 0},
+      tx: %ForgeAbi.Transaction{
+        chain_id: "",
+        delegator: "",
+        from: "",
+        gas: 0,
+        itx: %Google.Protobuf.Any{type_url: "", value: ""},
+        nonce: 0,
+        pk: "",
+        signature: "",
+        signatures: %ForgeAbi.Multisig{
+          data: %Google.Protobuf.Any{type_url: "", value: ""},
+          delegator: "",
+          pk: "",
+          signature: "",
+          signer: ""
+        }
+      }
+    }
   """
   def get_default(mod) do
     data = mod.new()
@@ -16,7 +54,8 @@ defmodule ForgeAbi.Util do
       props = data.__struct__.__message_props__
       field_mod = get_field_mod(props, key)
 
-      case Kernel.function_exported?(field_mod, :new, 0) do
+      case is_atom(field_mod) and Code.ensure_loaded?(field_mod) and
+             Kernel.function_exported?(field_mod, :new, 0) do
         true -> Map.put(data, key, get_default(field_mod))
         _ -> data
       end
--- test/forge_abi_test.exs
+++ test/forge_abi_test.exs
@@ -1,6 +1,8 @@
 defmodule ForgeAbiTest do
   use ExUnit.Case
+  doctest ForgeAbi.Util
   doctest ForgeAbi.Util.BigInt
   doctest ForgeAbi.Util.TypeUrl
+
   doctest ForgeAbi.Unit
 end
